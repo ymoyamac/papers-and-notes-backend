e5bbf9ea03068ca581596fea623902f2ccfe30f8
Yael Moya <yael.moya.mac@outlook.com>
2023-05-14 15:04:21 -0600

    feat: TypeORM connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_DATABASE_HOST,
+      port: Number(process.env.POSTGRES_DATABASE_PORT),
+      database: process.env.POSTGRES_DATABASE_NAME,
+      username: process.env.POSTGRES_DATABASE_USERNAME,
+      password: process.env.POSTGRES_DATABASE_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
